3d4e3a218e3865b0b2b2c8802e18fa5bbc904cba
Rinku97 <rinkurb0@gmail.com>
2021-03-04 02:34:33 +0530

    added product count feature based on category

--- Five_Cent/src/Five_Cent/views.py
+++ Five_Cent/src/Five_Cent/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
-from .models import Product, ProductImage
+from .models import Product, ProductImage, Category
 from django.core.paginator import Paginator
-
+from django.db.models import Count
 
 # Create your views here.
 
 def productlist(request):
     productlist = Product.objects.all()
+    categorylist = Category.objects.annotate(total_product=Count('product'))
     template = 'Product/product_list.html'
 
     paginator = Paginator(productlist, 1)  # Show 2 contacts per page.
     page_number = request.GET.get('page')
     productlist = paginator.get_page(page_number)
 
-    context = {'product_list': productlist}
+    context = {'product_list': productlist, 'category_list': categorylist}
     return render(request, template, context)
 
 
